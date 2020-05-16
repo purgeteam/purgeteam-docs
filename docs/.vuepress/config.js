92402b19a1b0a762c9d870fa8a4a9e852f3c3f75
Purgeyao <zhaoy@deepblueai.com>
2020-05-16 15:13:51 +0800

    feat(purgeteam-docs):第一版文档

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -2,18 +2,72 @@ module.exports = {
     base: '/',
     title: 'PurgeTeam',
     description: 'PurgeTeam blog docs',
+    head: [["link", {rel: "icon", href: `/favicon.ico`}]],
+    markdown: {
+        externalLinks: {
+            target: '_blank', rel: 'noopener noreferrer'
+        }
+    },
+    locales: {
+        // 键名是该语言所属的子路径
+        // 作为特例，默认语言可以使用 '/' 作为其路径。
+        '/zh/': {
+            lang: '简体中文',
+            title: 'PurgeTeam',
+            description: '中文'
+        },
+        '/': {
+            lang: 'English', // 将会被设置为 <html> 的 lang 属性
+            title: 'PurgeTeam',
+            description: '英文'
+        }
+    },
     themeConfig: {
         // 你的GitHub仓库，请正确填写
         repo: 'https://github.com/purgeteam/',
         // 自定义仓库链接文字。
         repoLabel: 'PurgeTeam GitHub',
         nav: [
-            { text: 'Home', link: '/' },
-            { text: 'FirstBlog', link: '/blog/FirstBlog.md' }
+            {
+                text: '首页', link: '/zh/'
+            },
+            {
+                text: '指南', link: '/zh/guide/unified-dispose-springboot-starter/'
+            },
+            {
+                text: '生态',
+                items: [
+                    {
+                        text: "全局处理(异常+返回)",
+                        link: "https://github.com/purgeteam/unified-dispose-springboot"
+                    }, {
+                        text: "swagger集成",
+                        link: "https://github.com/purgeteam/swagger-spring-boot"
+                    }, {
+                        text: "ElasticJob Starter",
+                        link: "https://github.com/purgeteam/elastic-job-spring-boot"
+                    }, {
+                        text: "动态配置监听",
+                        link: "https://github.com/purgeteam/dynamic-config-spring-boot"
+                    }, {
+                        text: "Mysql多数据源",
+                        link: "https://github.com/purgeteam/data-source-spring-boot"
+                    }, {
+                        text: "日志链路追踪",
+                        link: "https://github.com/purgeteam/log-trace-spring-boot"
+                    },
+                ]
+            },
+            {text: '个人博客', link: 'https://github.com/purgeyao.github.io'}
         ],
         sidebar: [
-            ['/', '首页'],
-            ['/blog/FirstBlog.md', '我的第一篇博客']
+            ['/zh/', '首页'],
+            ['/zh/guide/unified-dispose-springboot-starter/', '全局处理'],
+            ['/zh/guide/swagger-spring-boot/', 'Swagger集成'],
+            ['/zh/guide/elastic-job-spring-boot/', 'ElasticJob Plus'],
+            ['/zh/guide/dynamic-config-spring-boot/', '动态配置监听'],
+            ['/zh/guide/data-source-spring-boot/', 'Mysql多数据源'],
+            ['/zh/guide/log-trace-spring-boot/', '日志链路追踪']
         ]
     }
 }
--- docs/README.md
+++ docs/README.md
@@ -1,16 +1,16 @@
 ---
 home: true
-heroImage: /hero.png
-heroText: Hero 标题
-tagline: Hero 副标题
-actionText: 快速上手 →
+heroImage: /img/timg.jpeg
+heroText: Purge Team
+tagline: To develop efficiency
+actionText: Quick learning →
 actionLink: /zh/guide/
 features:
-- title: 简洁至上
-  details: 以 Markdown 为中心的项目结构，以最少的配置帮助你专注于写作。
-- title: Vue驱动
-  details: 享受 Vue + webpack 的开发体验，在 Markdown 中使用 Vue 组件，同时可以使用 Vue 来开发自定义主题。
-- title: 高性能
-  details: VuePress 为每个页面预渲染生成静态的 HTML，同时在页面被加载的时候，将作为 SPA 运行。
-footer: MIT Licensed | Copyright © 2018-present Evan You
+- title: Efficiency is the highest
+  details: Simple configuration can save a lot of time by quickly completing common requirements, global exception encapsulation, log tracking, elastic-job encapsulation, etc.
+- title: Quiet
+  details: The springboot integration is silky, without changing the richness of the functionality added to the existing framework.
+- title: Rich functionality
+  details: Global handling (exception + unification), ElasticJob Starter, swagger integration, dynamic configuration monitoring, Mysql multi-data source, microservice log link tracking.
+footer: Apache License 2.0 | © 2020 purgeteam
 ---
